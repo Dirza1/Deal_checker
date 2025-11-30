13111e95b40b6119c63e8218252efd2830b5cb73
jasper.olthof <jasper.olthof@thermofisher.com>
2025-11-30 12:04:52 +0100

    got it done!

--- main.py
+++ main.py
@@ -3,37 +3,57 @@ import time
 from collections import defaultdict
 from bs4 import BeautifulSoup
 import re
+import datetime
 
 def main(playwright: Playwright):
     supermarket_url:defaultdict[str,str] = defaultdict(str,{
-    "ah": "https://www.supermarktaanbiedingen.com/albert-heijn",
-    "jumbo": "https://www.supermarktaanbiedingen.com/jumbo",
-    "poiesz": "https://www.supermarktaanbiedingen.com/poiesz",
-    "lidl": "https://www.supermarktaanbiedingen.com/lidl",
-    "aldi": "https://www.supermarktaanbiedingen.com/aldi",
+    "ah": "https://www.supermarktaanbiedingen.com/aanbiedingen/albert_heijn",
+    "jumbo": "https://www.supermarktaanbiedingen.com/aanbiedingen/jumbo",
+    "poiesz": "https://www.supermarktaanbiedingen.com/aanbiedingen/poiesz",
+    "lidl": "https://www.supermarktaanbiedingen.com/aanbiedingen/lidl",
+    "aldi": "https://www.supermarktaanbiedingen.com/aanbiedingen/aldi",
 })
+    with open("te_vinden.log",mode="r") as te_zoeken:
+        acties = te_zoeken.read()
+        te_zoeken.close()
+    
+    zoek_lijst = acties.split(sep=",")
+    print(len(zoek_lijst))
 
     chrome = playwright.chromium
     brouwser = chrome.launch(headless=True)
-    page = brouwser.new_page()
-    page.goto("https://www.supermarktaanbiedingen.com/aanbiedingen/albert_heijn",wait_until="networkidle")
-
-    soup = BeautifulSoup(page.content(), "html.parser")
-    aanbiedingen = soup.find_all(id=re.compile("product-"))
-    print(len(aanbiedingen))
     with open("aanbiedingen",mode="w") as file:
-        for aanbieding in aanbiedingen:
-            Aanbieding_tietel = aanbieding.h3
-            Aanbieding_text = aanbieding.p
-            Oud_nieuw = aanbieding.find_all("span")
-            if "beemster" in Aanbieding_tietel.text.lower():
-                if len(Oud_nieuw) == 2:
-                    file.write(f"Aanbieding: {Aanbieding_tietel.text}.\n Aanbieding text: {Aanbieding_text.text}.\n Oude Prijs: {Oud_nieuw[0].text}.\n Prijs nieuw: {Oud_nieuw[1].text if Oud_nieuw[1].text else None}.\n")
-                    file.write("\n")
-                else:
-                    file.write(f"Aanbieding: {Aanbieding_tietel.text}.\n Aanbieding text: {Aanbieding_text.text}.\n")
-                    file.write("\n")
+        for supermarked,url in supermarket_url.items():
+            page = brouwser.new_page()
+            file.write(f"AANBIEDIGEN VAN {supermarked.upper()}!\n")
+            print(f"Supermarkt = {supermarked}, URL = {url}")
+            page.goto(url=url,wait_until="networkidle")
+            soup = BeautifulSoup(page.content(), "html.parser")
+            aanbiedingen = soup.find_all(id=re.compile("product-"))
+            print(len(aanbiedingen))
+            
+            for aanbieding in aanbiedingen:
+                Aanbieding_tietel = aanbieding.h3
+                Aanbieding_text = aanbieding.p
+                if not Aanbieding_tietel or not Aanbieding_text:
+                    continue
+                Oud_nieuw = aanbieding.find_all("span")
+                for actie in zoek_lijst:
+                    if actie in Aanbieding_tietel.text.lower():
+                        if len(Oud_nieuw) == 2:
+                            file.write(f"Aanbieding: {Aanbieding_tietel.text}.\n"
+                                    f"Aanbieding text: {Aanbieding_text.text}.\n"
+                                    f"Oude Prijs: {Oud_nieuw[0].text}.\n "
+                                    f"Prijs nieuw: {Oud_nieuw[1].text if Oud_nieuw[1].text else "Onbekend"}.\n")                        
+                            file.write("\n")
+                        else:
+                            file.write(f"Aanbieding: {Aanbieding_tietel.text}.\n"
+                                    f"Aanbieding text: {Aanbieding_text.text}.\n")
+                            file.write("\n")
+
+    brouwser.close()
 
 if __name__ == "__main__":
-    with sync_playwright() as playwright:
-        main(playwright=playwright)
+    #if datetime.date.today().day == 0:
+     with sync_playwright() as playwright:
+        main(playwright=playwright) 
