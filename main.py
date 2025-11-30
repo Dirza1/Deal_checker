efd40176a7b32dfc344ac27b9d9417f2acad11d7
jasper.olthof <jasper.olthof@thermofisher.com>
2025-11-30 12:12:58 +0100

    last bugfix

--- main.py
+++ main.py
@@ -13,16 +13,15 @@ def main(playwright: Playwright):
     "lidl": "https://www.supermarktaanbiedingen.com/aanbiedingen/lidl",
     "aldi": "https://www.supermarktaanbiedingen.com/aanbiedingen/aldi",
 })
-    with open("te_vinden.log",mode="r") as te_zoeken:
+    with open("te_vinden.log",mode="r",encoding="utf-8") as te_zoeken:
         acties = te_zoeken.read()
-        te_zoeken.close()
     
     zoek_lijst = acties.split(sep=",")
     print(len(zoek_lijst))
 
     chrome = playwright.chromium
     brouwser = chrome.launch(headless=True)
-    with open("aanbiedingen",mode="w") as file:
+    with open("aanbiedingen",mode="w",encoding="utf-8") as file:
         for supermarked,url in supermarket_url.items():
             page = brouwser.new_page()
             file.write(f"AANBIEDIGEN VAN {supermarked.upper()}!\n")
@@ -44,16 +43,17 @@ def main(playwright: Playwright):
                             file.write(f"Aanbieding: {Aanbieding_tietel.text}.\n"
                                     f"Aanbieding text: {Aanbieding_text.text}.\n"
                                     f"Oude Prijs: {Oud_nieuw[0].text}.\n "
-                                    f"Prijs nieuw: {Oud_nieuw[1].text if Oud_nieuw[1].text else "Onbekend"}.\n")                        
+                                    f"Prijs nieuw: {Oud_nieuw[1].text if Oud_nieuw[1].text else 'Onbekend'}.\n")                        
                             file.write("\n")
                         else:
                             file.write(f"Aanbieding: {Aanbieding_tietel.text}.\n"
                                     f"Aanbieding text: {Aanbieding_text.text}.\n")
                             file.write("\n")
+                page.close()
 
     brouwser.close()
 
 if __name__ == "__main__":
-    #if datetime.date.today().day == 0:
-     with sync_playwright() as playwright:
-        main(playwright=playwright) 
+    #if datetime.date.today().weekday() == 0:
+        with sync_playwright() as playwright:
+            main(playwright=playwright) 
